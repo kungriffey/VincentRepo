04fed10ba5b4f7a48785d6d405afe8fc26af9ea7
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-08 09:28:06 -0400

    added Kunwar Logic

--- VincentRepo/main.m
+++ VincentRepo/main.m
@@ -11,7 +11,7 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Hello Vincent! it's your neighbour, Kunwar!");
     }
     return 0;
 }
